1da3fb6095dbb52d1b5ea2300ac1ec99f41873d8
ojilon <ojilongkefa@gmail.com>
2026-06-03 20:25:33 +0300

    initial commit, git ignore added

---
+++ app/src/main/java/com/example/wayer/Config.java
@@ -0,0 +1,7 @@
+package com.example.wayer;
+
+public class Config {
+    // Put your PC's actual hotspot IP address here [cite: 27, 28]
+    public static final String HOST = "192.168.43.41"; 
+    public static final int PORT = 5000;
+}
