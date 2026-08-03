ec8f0bfc9265323cd62715056dc3185a9d42a342
ojilon <ojilongkefa@gmail.com>
2026-08-03 20:44:09 +0300

    connect the adapter to filesfragment.java

--- app/src/main/java/com/example/wayer/ui/FilesFragment.java
+++ app/src/main/java/com/example/wayer/ui/FilesFragment.java
@@ -1,11 +1,64 @@
 package com.example.wayer.ui;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wayer.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilesFragment extends Fragment {
+
     public FilesFragment() {
         super(R.layout.fragment_files);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView fileList = view.findViewById(R.id.file_list);
+
+        fileList.setLayoutManager( new LinearLayoutManager(requireContext()) );
+
+        List<FileItem> files = new ArrayList<>();
+
+        files.add( new FileItem( "Documents", "Folder", true) );
+
+        files.add(
+                new FileItem(
+                        "Download",
+                        "Folder",
+                        true
+                )
+        );
+
+        files.add(
+                new FileItem(
+                        "Pictures",
+                        "Folder",
+                        true
+                )
+        );
+
+        files.add(
+                new FileItem(
+                        "notes.txt",
+                        "12 KB",
+                        false
+                )
+        );
+
+        FileAdapter adapter = new FileAdapter(files);
+
+        fileList.setAdapter(adapter);
     }
 }
